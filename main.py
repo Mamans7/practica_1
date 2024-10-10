6d01a3a1ec0099e139e6e1e680b7304d0ab2a657
Pedrini <psmarnilag@gmail.com>
2024-10-10 13:11:17 -0400

    Creando archivos templates y armando de a poco

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from flask import Flask, request, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
   return render_template('index.html')
 
+@app.route('/quienes')
+def quienes():
+  return render_template('quienes.html')
 
+@app.route('/servicios')
+def servicios():
+  return render_template('servicios.html')
 
+# @app.noticias('/noticias')
+# def noticias():
+#   return render_template('noticias.html')
 
-
-
-
-
+@app.route('/contactos')
+def contactos():
+  return render_template('contactos.html')
 
 
 
